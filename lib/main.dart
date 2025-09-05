5f90266d4c6aa1d1700a99c1710fa1aa8f073fea
DeveloperKhurramNaseem <developerkhurramnaseem@gmail.com>
2025-09-05 14:14:41 +0500

    feat: Home screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:help_mee/presentation/screens/home/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(            
+    return MaterialApp(      
+      home: const HomeScreen(),      
     );
   }
 }
